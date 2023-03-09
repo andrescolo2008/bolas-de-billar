1d27e20740b48bae7c7647b006918f351bb67d9c
Andrés Correa <andrescolo2008@gmail.com>
2023-03-09 10:31:20 -0500

    imágnes de bolas y variables

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,8 +5,11 @@
  * 2S = Two of Spades
  */
 
-let deck         = [];
-const tipos      = ['C','D','H','S'];
+// let deck         = [];
+let conjunto= [];
+// const tipos      = ['C','D','H','S'];
+let bolas=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
 const especiales = ['A','J','Q','K'];
 
 let puntosJugador = 0,
